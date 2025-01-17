546d9a74f27007a0dd4761974ad30a2cf8c85d60
UooUzzz <uuumonazzz@gmail.com>
2025-01-17 20:27:07 +0900

    feat: google-login-data-source-impl

--- lib/data/source/google_login_data_source.dart
+++ lib/data/source/google_login_data_source.dart
@@ -6,7 +6,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 abstract interface class GoogleLoginDataSource {
   Future<GoogleSignInAccount?> signInWithGoogle(); // google 계정으로 로그인
   
-  Future<UserCredential> signInWithFirebase(OAuthCredential credenrial); // firebase 인증
+  Future<UserCredential> signInWithFirebase(OAuthCredential credential); // firebase 인증
 
   Future<QuerySnapshot> fetchUserEmail(String email); // 이메일로 기존 사용자 검색
 
