782a5924e673a31d8bda1939cdc3c10083d76f53
marion419 <daynvi@gmail.com>
2025-01-22 16:59:43 +0900

    refactor: PlayListSource 파일 위치 firebase 폴더 안으로 변경

--- lib/data/source/firebase/play_list_source.dart
+++ lib/data/source/firebase/play_list_source.dart
@@ -2,7 +2,7 @@ import 'package:oz_player/data/dto/play_list_dto.dart';
 
 abstract interface class PlayListSource {
   Future<List<PlayListDTO>> getPlayLists(String userId);
-  Future<PlayListDTO> getPlayList(String userId, String listName);
+  Future<PlayListDTO?> getPlayList(String userId, String listName);
   Future<void> addPlayList(String userId, PlayListDTO playListDTO);
   Future<void> addSong(String userId, String listName, String songId);
 }
