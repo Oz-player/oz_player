47ebde185e2e6c4abcd7ba69470f6c5be37774ef
marion419 <daynvi@gmail.com>
2025-01-22 18:24:34 +0900

    feat: PlayListUsecases

---
+++ lib/domain/usecase/play_list_usecase.dart
@@ -0,0 +1,42 @@
+import 'package:oz_player/data/dto/play_list_dto.dart';
+import 'package:oz_player/domain/repository/play_list_repository.dart';
+
+class PlayListUsecase {
+  final PlayListRepository _repository;
+
+  PlayListUsecase(this._repository);
+
+  // userId가 쓰이는 부분은 현재 유저 정보 Provider가 생기면
+  // 해당 유저의 아이디로 변경하겠습니다
+  Future<List<PlayListDTO>> getPlayLists(String userId) async {
+    return await _repository.getPlayLists(userId);
+  }
+
+  Future<PlayListDTO?> getPlayList(String userId, String listName) async {
+    return await _repository.getPlayList(userId, listName);
+  }
+
+  Future<void> addPlayList(String userId, PlayListDTO playListDTO) async {
+    await _repository.addPlayList(userId, playListDTO);
+  }
+
+  Future<void> addSong(String userId, String listName, String songId) async {
+    await _repository.addSong(userId, listName, songId);
+  }
+
+  Future<void> deletePlayList(String userId, String listName) async {
+    await _repository.deletePlayList(userId, listName);
+  }
+
+  Future<void> deleteSong(
+    String userId,
+    String listName,
+    String songId,
+  ) async {
+    await _repository.deleteSong(
+      userId,
+      listName,
+      songId,
+    );
+  }
+}
