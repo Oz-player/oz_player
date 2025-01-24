1749ef04e55154cb6990d6d5281ced7a4736e94a
marion419 <daynvi@gmail.com>
2025-01-24 19:14:12 +0900

    add: SavedPage 탭 변경 버튼

--- lib/presentation/ui/saved/saved_page.dart
+++ lib/presentation/ui/saved/saved_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:oz_player/presentation/ui/saved/widgets/saved_tab_button.dart';
+import 'package:oz_player/presentation/widgets/home_tap/home_bottom_navigation.dart';
 
 class SavedPage extends StatefulWidget {
   const SavedPage({super.key});
@@ -15,35 +17,50 @@ class _SavedPageState extends State<SavedPage> {
     super.initState();
   }
 
+  void onButtonClicked() {
+    setState(() {
+      isLibrary = !isLibrary;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('보관함'),
+        title: Text(
+          '보관함',
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: Row(
+      body: Column(
         children: [
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                isLibrary = true;
-              });
-            },
-            child: Text(
-              'Library',
+          // 탭 이동 버튼
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                SavedTabButton(
+                  title: '라이브러리',
+                  isLibrary: isLibrary,
+                  onClicked: onButtonClicked,
+                ),
+                SavedTabButton(
+                  title: '플레이리스트',
+                  isLibrary: !isLibrary,
+                  onClicked: onButtonClicked,
+                ),
+                Text('$isLibrary'),
+              ],
             ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                isLibrary = false;
-              });
-            },
-            child: Text('playlist'),
-          ),
-          Text('$isLibrary'),
+          // 정렬 기준
+          // 내용
         ],
       ),
+      bottomNavigationBar: HomeBottomNavigation(),
     );
   }
 }
