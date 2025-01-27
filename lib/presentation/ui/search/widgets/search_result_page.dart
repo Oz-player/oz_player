90e591e47448c5fda34fdb9f122681e6b8c06939
chabugon <qnrhs0913@gmail.com>
2025-01-27 16:39:57 +0900

    feat : spotify search 연동, search ui에 검색 연결

--- lib/presentation/ui/search/widgets/search_result_page.dart
+++ lib/presentation/ui/search/widgets/search_result_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:oz_player/presentation/ui/search/search_view_model.dart';
 
-class SearchResultPage extends StatefulWidget {
+class SearchResultPage extends ConsumerStatefulWidget {
   const SearchResultPage({super.key});
 
   @override
-  State<SearchResultPage> createState() => _SearchResultPageState();
+  ConsumerState<SearchResultPage> createState() => _SearchResultPageState();
 }
 
-class _SearchResultPageState extends State<SearchResultPage> {
+class _SearchResultPageState extends ConsumerState<SearchResultPage> {
   bool titleButton = true;
   bool lyricsButton = false;
 
@@ -25,6 +27,16 @@ class _SearchResultPageState extends State<SearchResultPage> {
 
   @override
   Widget build(BuildContext context) {
+ final searchResults = ref.watch(searchpageListViewModel);
+
+if (searchResults == null) {
+  return const Center(child: CircularProgressIndicator());
+}
+
+if (searchResults.isEmpty) {
+  return const Center(child: Text('검색 결과가 없습니다.'));
+}
+
     return SafeArea(
       child: Column(
         children: [
@@ -75,8 +87,9 @@ class _SearchResultPageState extends State<SearchResultPage> {
           ),
           Expanded(
             child: ListView.separated(
-              itemCount: 10,
+              itemCount: searchResults?.length ?? 0,
               itemBuilder: (context, index) {
+                final result = searchResults![index]; // SpotifyEntity
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: Row(
@@ -89,13 +102,14 @@ class _SearchResultPageState extends State<SearchResultPage> {
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              '음악 제목$index',
+                              result.name, // SpotifyEntity의 title 속성 사용
                               style: TextStyle(
                                   fontSize: 16, fontWeight: FontWeight.bold),
                             ),
-                            Text('가수 이름', style: TextStyle(fontSize: 16)),
+                            Text(result.type, style: TextStyle(fontSize: 16)), // SpotifyEntity의 artist 속성 사용
                           ],
                         ),
                       ),
