b7ece1a9872da4b6d09d158c3de54abfbc60c8ad
marion419 <daynvi@gmail.com>
2025-01-27 16:33:50 +0900

    refactor: PlayList 위젯 파일 분리

--- lib/presentation/ui/saved/saved_page.dart
+++ lib/presentation/ui/saved/saved_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oz_player/presentation/ui/saved/view_models/playlist_view_model.dart';
+import 'package:oz_player/presentation/ui/saved/widgets/play_list.dart';
 import 'package:oz_player/presentation/ui/saved/widgets/saved_tab_button.dart';
 import 'package:oz_player/presentation/widgets/home_tap/home_bottom_navigation.dart';
 
@@ -108,78 +109,15 @@ class _SavedPageState extends ConsumerState<SavedPage> {
               height: 12,
             ),
             // 내용
-            SizedBox(
-              width: double.infinity,
-              height: 586,
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) => Container(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  height: 80,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            // 플레이리스트 대표 이미지
-                            Container(
-                              width: 56,
-                              height: 56,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(4),
-                                color: Colors.grey[600],
-                              ),
-                            ),
-                            // 플레이리스트 내용
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 18),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      '플레이리스트 이름 플레이리스트 이름',
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w600,
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                    Text(
-                                      '00개의 곡',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      // 메뉴 버튼
-                      GestureDetector(
-                        onTap: () {
-                          print('tap');
-                        },
-                        child: Container(
-                          width: 44,
-                          height: 44,
-                          color: Colors.transparent,
-                          child: Icon(Icons.more_vert),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            isLibrary
+                ? SizedBox(
+                    width: double.infinity,
+                    height: 586,
+                    child: GridView.count(
+                      crossAxisCount: 3,
+                    ),
+                  )
+                : PlayList(),
           ],
         ),
       ),
