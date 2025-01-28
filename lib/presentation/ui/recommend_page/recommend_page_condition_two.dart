e4417512fa276b111abcbb4de4fe0a8435c9a79f
Hamiric <do9870@naver.com>
2025-01-28 18:12:07 +0900

    [Fix] : audioPlayer UI 변경#5

--- lib/presentation/ui/recommend_page/recommend_page_condition_two.dart
+++ lib/presentation/ui/recommend_page/recommend_page_condition_two.dart
@@ -1,7 +1,6 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:oz_player/presentation/ui/recommend_page/view_model/card_position_provider.dart';
 import 'package:oz_player/presentation/ui/recommend_page/view_model/condition_view_model.dart';
 import 'package:oz_player/presentation/ui/recommend_page/view_model/save_song_bottom_sheet_view_model.dart';
