058477fe624f75b364f0775f8ab99cd66d49a2d9
marion419 <daynvi@gmail.com>
2025-01-31 04:25:28 +0900

    feat: RawSong

---
+++ lib/domain/repository/saved/play_list_repository.dart
@@ -0,0 +1,11 @@
+import 'package:oz_player/data/dto/play_list_dto.dart';
+import 'package:oz_player/domain/entitiy/play_list_entity.dart';
+
+abstract interface class PlayListRepository {
+  Future<List<PlayListEntity>> getPlayLists(String userId);
+  Future<PlayListEntity?> getPlayList(String userId, String listName);
+  Future<void> addPlayList(String userId, PlayListDTO playListDTO);
+  Future<void> addSong(String userId, String listName, String songId);
+  Future<void> deletePlayList(String userId, String listName);
+  Future<void> deleteSong(String userId, String listName, String songId);
+}
