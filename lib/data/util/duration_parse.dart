882e95465aa7ead979be970f37809e92151449ab
marion419 <daynvi@gmail.com>
2025-01-31 20:24:55 +0900

    fix: duration 파싱 시 오류 수정

--- lib/data/util/duration_parse.dart
+++ lib/data/util/duration_parse.dart
@@ -3,6 +3,6 @@ Duration durationParse(String target) {
   return Duration(
     hours: int.parse(parts[0]),
     minutes: int.parse(parts[1]),
-    seconds: int.parse(parts[2]),
+    seconds: int.parse(parts[2].split('.')[0]),
   );
 }
