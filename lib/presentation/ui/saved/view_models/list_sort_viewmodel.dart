f37bde876d16a9f5b0f64fcf2df061a553735185
marion419 <daynvi@gmail.com>
2025-02-02 19:07:24 +0900

    feat: 라이브러리 정렬 기능

--- lib/presentation/ui/saved/view_models/list_sort_viewmodel.dart
+++ lib/presentation/ui/saved/view_models/list_sort_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:oz_player/presentation/ui/saved/view_models/library_view_model.dart';
 import 'package:oz_player/presentation/ui/saved/view_models/playlist_view_model.dart';
 
 enum SortedType {
@@ -18,11 +19,13 @@ class ListSortViewmodel extends Notifier<SortedType> {
   void setLatest() {
     state = SortedType.latest;
     ref.read(playListViewModelProvider.notifier).getPlayListsLatest();
+    ref.read(libraryViewModelProvider.notifier).getLibraryLatest();
   }
 
   void setAscending() {
     state = SortedType.ascending;
     ref.read(playListViewModelProvider.notifier).getPlayListsAscending();
+    ref.read(libraryViewModelProvider.notifier).getLibraryAscending();
   }
 }
 
