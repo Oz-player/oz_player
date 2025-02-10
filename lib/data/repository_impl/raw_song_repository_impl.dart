4b42df9a663d8a49419f2d22038cf2508f3df4e5
marion419 <daynvi@gmail.com>
2025-02-10 14:06:28 +0900

    feat: ranking data 가져오는 로직

--- lib/data/repository_impl/raw_song_repository_impl.dart
+++ lib/data/repository_impl/raw_song_repository_impl.dart
@@ -1,6 +1,8 @@
+import 'dart:developer';
 import 'package:oz_player/data/dto/raw_song_dto.dart';
 import 'package:oz_player/data/source/firebase_songs/raw_song_source.dart';
 import 'package:oz_player/domain/entitiy/raw_song_entity.dart';
+import 'package:oz_player/domain/entitiy/video_info_entitiy.dart';
 import 'package:oz_player/domain/repository/saved/raw_song_repository.dart';
 
 class RawSongRepositoryImpl implements RawSongRepository {
@@ -34,5 +36,55 @@ class RawSongRepositoryImpl implements RawSongRepository {
   @override
   Future<void> updateVideo(RawSongDto dto) async {
     await _source.updateVideo(dto);
+  }
+
+  @override
+  Future<List<RawSongDto>> getCardRanking() async {
+    final sourceList = await _source.getCardRanking();
+    final dummy = RawSongDto(
+      countLibrary: 0,
+      countPlaylist: 0,
+      video: VideoInfoEntitiy.empty(),
+      title: '데이터가 없습니다.',
+      imgUrl:
+          'https://github.com/user-attachments/assets/b12b1ec8-3646-4b66-bd87-e7470f3d833e',
+      artist: '데이터가 없습니다',
+    );
+
+    List<RawSongDto> resultList = sourceList.reversed.toList();
+    resultList.sort((a, b) => a.countLibrary.compareTo(b.countLibrary));
+    // 곡이 3개 미만인 경우 더미 데이터로 나머지를 채움
+    while (resultList.length < 3) {
+      resultList.add(dummy);
+    }
+    for (var item in resultList) {
+      log('title: ${item.title}, count: ${item.countLibrary}');
+    }
+    return resultList;
+  }
+
+  @override
+  Future<List<RawSongDto>> getPlaylistRanking() async {
+    final sourceList = await _source.getPlaylistRanking();
+    final dummy = RawSongDto(
+      countLibrary: 0,
+      countPlaylist: 0,
+      video: VideoInfoEntitiy.empty(),
+      title: '데이터가 없습니다.',
+      imgUrl:
+          'https://github.com/user-attachments/assets/b12b1ec8-3646-4b66-bd87-e7470f3d833e',
+      artist: '데이터가 없습니다',
+    );
+
+    List<RawSongDto> resultList = sourceList.reversed.toList();
+    resultList.sort((a, b) => b.countPlaylist.compareTo(a.countPlaylist));
+    // 곡이 3개 미만인 경우 더미 데이터로 나머지를 채움
+    while (resultList.length < 3) {
+      resultList.add(dummy);
+    }
+    for (var item in resultList) {
+      log('title: ${item.title}, count: ${item.countLibrary}');
+    }
+    return resultList;
   }
 }
--- lib/data/source/firebase_songs/raw_song_source_impl.dart
+++ lib/data/source/firebase_songs/raw_song_source_impl.dart
@@ -103,4 +103,54 @@ class RawSongSourceImpl implements RawSongSource {
       print('video를 업데이트하였습니다.');
     }
   }
+
+  // --------------------------------------------------------------------
+  // 랭킹 관련 기능
+  // --------------------------------------------------------------------
+
+  // 카드 저장 순으로 상위 3개 가져오는 함수
+  @override
+  Future<List<RawSongDto>> getCardRanking() async {
+    try {
+      final querySnapshot = await _firestore
+          .collection('Song')
+          .orderBy('countLibrary', descending: true)
+          .limit(3)
+          .get();
+      final docs = querySnapshot.docs;
+      if (docs.isNotEmpty) {
+        return docs.map((e) {
+          return RawSongDto.fromJson(e.data());
+        }).toList();
+      }
+
+      return [];
+    } catch (e, stackTrace) {
+      print('e: $e, stackTrace: $stackTrace');
+      return [];
+    }
+  }
+
+  // 플레이리스트 저장 순으로 상위 3개 가져오는 함수
+  @override
+  Future<List<RawSongDto>> getPlaylistRanking() async {
+    try {
+      final querySnapshot = await _firestore
+          .collection('Song')
+          .orderBy('countPlaylist', descending: true)
+          .limit(3)
+          .get();
+      final docs = querySnapshot.docs;
+      if (docs.isNotEmpty) {
+        return docs.map((e) {
+          return RawSongDto.fromJson(e.data());
+        }).toList();
+      }
+
+      return [];
+    } catch (e, stackTrace) {
+      print('e: $e, stackTrace: $stackTrace');
+      return [];
+    }
+  }
 }
